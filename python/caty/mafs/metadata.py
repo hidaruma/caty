750b6b214df167802dd42f475eb2874325bbb213
ckuwata <devnull@localhost>
2011-12-15 10:24:31 +0900

    content-typeを整理

--- python/caty/mafs/metadata.py
+++ python/caty/mafs/metadata.py
@@ -164,245 +164,47 @@ class DirectoryEntry(object):
 
 
 default_types = {
-    u".obj": {
-         
-        u"isText": False, 
-        u"contentType": u"application/octet-stream"
-    }, 
-    u".ra": {
-         
-        u"isText": False, 
-        u"contentType": u"audio/x-pn-realaudio"
-    }, 
-    u".wsdl": {
-         
-        u"isText": False, 
-        u"contentType": u"application/xml"
-    }, 
-    u".dll": {
-         
-        u"isText": False, 
-        u"contentType": u"application/octet-stream"
-    }, 
-    u".ras": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-cmu-raster"
-    }, 
-    u".ram": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-pn-realaudio"
-    }, 
-    u".bcpio": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-bcpio"
-    }, 
-    u".m1v": {
-         
-        u"isText": False, 
-        u"contentType": u"video/mpeg"
-    }, 
-    u".xwd": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-xwindowdump"
-    }, 
-    u".avi": {
-         
-        u"isText": False, 
-        u"contentType": u"video/x-msvideo"
-    }, 
-    u".bmp": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-ms-bmp"
-    }, 
-    u".shar": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-shar"
-    }, 
     u".js": {
-         
         u"isText": True, 
         u"contentType": u"application/x-javascript"
     }, 
-    u".dvi": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-dvi"
-    }, 
-    u".aif": {
-         
-        u"isText": False, 
-        u"contentType": u"audio/x-aiff"
-    }, 
-    u".ksh": {
-         
-        u"isText": True, 
-        u"contentType": u"text/plain"
-    }, 
-    u".dot": {
-         
-        u"isText": False, 
-        u"contentType": u"application/msword"
-    }, 
-    u".mht": {
-         
-        u"isText": True, 
-        u"contentType": u"message/rfc822"
-    }, 
-    u".vcf": {
-         
-        u"isText": False, 
-        u"contentType": u"text/x-vcard"
-    }, 
     u".css": {
-         
         u"isText": True, 
         u"contentType": u"text/css"
     }, 
-    u".csh": {
-         
-        u"isText": True, 
-        u"contentType": u"application/x-csh"
-    }, 
-    u".pwz": {
-         
-        u"isText": False, 
-        u"contentType": u"application/vnd.ms-powerpoint"
-    }, 
     u".pdf": {
-         
         u"isText": False, 
         u"contentType": u"application/pdf"
     }, 
-    u".cdf": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-netcdf"
-    }, 
-    u".pl": {
-        u"isText": True, 
-        u"contentType": u"text/plain"
-    }, 
-    u".p12": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-pkcs12"
-    }, 
     u".jpe": {
-         
         u"isText": False, 
         u"contentType": u"image/jpeg"
     }, 
     u".jpg": {
-         
         u"isText": False, 
         u"contentType": u"image/jpeg"
-    }, 
-    u".py": {
-         
-        u"isText": True, 
-        u"contentType": u"text/plain"
     }, 
     u".xml": {
         u"isText": True, 
         u"contentType": u"text/xml"
     }, 
     u".jpeg": {
-         
         u"isText": False, 
         u"contentType": u"image/jpeg"
     }, 
     u".ps": {
-         
         u"isText": False, 
         u"contentType": u"application/postscript"
-    }, 
-    u".gtar": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-gtar"
-    }, 
-    u".tif": {
-         
-        u"isText": False, 
-        u"contentType": u"image/tiff"
-    }, 
-    u".hdf": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-hdf"
-    }, 
-    u".nws": {
-         
-        u"isText": False, 
-        u"contentType": u"message/rfc822"
     }, 
     u".tsv": {
         u"isText": True, 
         u"contentType": u"text/tab-separated-values"
     }, 
-    u".xpdl": {
-         
-        u"isText": True, 
-        u"contentType": u"application/xml"
-    }, 
-    u".src": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-wais-source"
-    }, 
-    u".sh": {
-         
-        u"isText": True, 
-        u"contentType": u"application/x-sh"
-    }, 
-    u".p7c": {
-         
-        u"isText": False, 
-        u"contentType": u"application/pkcs7-mime"
-    }, 
-    u".ief": {
-         
-        u"isText": False, 
-        u"contentType": u"image/ief"
-    }, 
-    u".so": {
-         
-        u"isText": False, 
-        u"contentType": u"application/octet-stream"
-    }, 
-    u".xlb": {
-         
-        u"isText": False, 
-        u"contentType": u"application/vnd.ms-excel"
-    }, 
-    u".pbm": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-portable-bitmap"
-    }, 
-    u".texinfo": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-texinfo"
-    }, 
-    u".xls": {
-         
-        u"isText": False, 
-        u"contentType": u"application/vnd.ms-excel"
-    }, 
-    u".tex": {
-         
+    u".caty": {
         u"isText": True, 
         u"contentType": u"application/x-tex"
     }, 
     u".gif": {
-         
         u"isText": False, 
         u"contentType": u"image/gif"
     }, 
@@ -410,263 +212,26 @@ default_types = {
         u"isText": True, 
         u"contentType": u"text/html"
     }, 
-    u".aiff": {
-         
-        u"isText": False, 
-        u"contentType": u"audio/x-aiff"
-    }, 
-    u".aifc": {
-         
-        u"isText": False, 
-        u"contentType": u"audio/x-aiff"
-    }, 
-    u".exe": {
-         
-        u"isText": False, 
-        u"contentType": u"application/octet-stream"
-    }, 
-    u".sgm": {
-         
-        u"isText": False, 
-        u"contentType": u"text/x-sgml"
-    }, 
-    u".xpm": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-xpixmap"
-    }, 
     u".mpeg": {
-         
         u"isText": False, 
         u"contentType": u"video/mpeg"
-    }, 
-    u".ms": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-troff-ms"
-    }, 
-    u".rtx": {
-         
-        u"isText": True, 
-        u"contentType": u"text/richtext"
-    }, 
-    u".ppt": {
-         
-        u"isText": False, 
-        u"contentType": u"application/vnd.ms-powerpoint"
-    }, 
-    u".pps": {
-         
-        u"isText": False, 
-        u"contentType": u"application/vnd.ms-powerpoint"
-    }, 
-    u".sgml": {
-         
-        u"isText": False, 
-        u"contentType": u"text/x-sgml"
-    }, 
-    u".ppm": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-portable-pixmap"
-    }, 
-    u".latex": {
-         
-        u"isText": True, 
-        u"contentType": u"application/x-latex"
-    }, 
-    u".bat": {
-         
-        u"isText": True, 
-        u"contentType": u"text/plain"
-    }, 
-    u".mov": {
-         
-        u"isText": False, 
-        u"contentType": u"video/quicktime"
-    }, 
-    u".ppa": {
-         
-        u"isText": False, 
-        u"contentType": u"application/vnd.ms-powerpoint"
-    }, 
-    u".rgb": {
-        u"isText": False, 
-        u"contentType": u"image/x-rgb"
     }, 
     u".rdf": {
         u"isText": True, 
         u"contentType": u"application/rdf+xml"
-    }, 
-    u".xsl": {
-         
-        u"isText": True, 
-        u"contentType": u"application/xml"
-    }, 
-    u".eml": {
-         
-        u"isText": False, 
-        u"contentType": u"message/rfc822"
-    }, 
-    u".ai": {
-         
-        u"isText": False, 
-        u"contentType": u"application/postscript"
-    }, 
-    u".nc": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-netcdf"
-    }, 
-    u".sv4cpio": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-sv4cpio"
-    }, 
-    u".bin": {
-         
-        u"isText": False, 
-        u"contentType": u"application/octet-stream"
-    }, 
-    u".h": {
-         
-        u"isText": True, 
-        u"contentType": u"text/plain"
-    }, 
-    u".tcl": {
-         
-        u"isText": True, 
-        u"contentType": u"application/x-tcl"
-    }, 
-    u".wiz": {
-         
-        u"isText": False, 
-        u"contentType": u"application/msword"
-    }, 
-    u".o": {
-         
-        u"isText": False, 
-        u"contentType": u"application/octet-stream"
-    }, 
-    u".a": {
-         
-        u"isText": False, 
-        u"contentType": u"application/octet-stream"
-    }, 
-    u".c": {
-         
-        u"isText": True, 
-        u"contentType": u"text/plain"
     }, 
     u".wav": {
          
         u"isText": False, 
         u"contentType": u"audio/x-wav"
     }, 
-    u".xbm": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-xbitmap"
-    }, 
-    u".txt": {
-         
-        u"isText": True, 
-        u"contentType": u"text/plain"
-    }, 
-    u".au": {
-         
-        u"isText": False, 
-        u"contentType": u"audio/basic"
-    }, 
-    u".eps": {
-         
-        u"isText": False, 
-        u"contentType": u"application/postscript"
-    }, 
-    u".t": {
-         
-        u"isText": True, 
-        u"contentType": u"application/x-troff"
-    }, 
-    u".tiff": {
-         
-        u"isText": False, 
-        u"contentType": u"image/tiff"
-    }, 
-    u".texi": {
-         
-        u"isText": True, 
-        u"contentType": u"application/x-texinfo"
-    }, 
-    u".oda": {
-         
-        u"isText": False, 
-        u"contentType": u"application/oda"
-    }, 
-    u".ustar": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-ustar"
-    }, 
-    u".tr": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-troff"
-    }, 
-    u".me": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-troff-me"
-    }, 
-    u".sv4crc": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-sv4crc"
-    }, 
-    u".qt": {
-         
-        u"isText": False, 
-        u"contentType": u"video/quicktime"
-    }, 
-    u".mpa": {
-         
-        u"isText": False, 
-        u"contentType": u"video/mpeg"
-    }, 
     u".mpg": {
-         
         u"isText": False, 
         u"contentType": u"video/mpeg"
     }, 
     u".mpe": {
-         
         u"isText": False, 
         u"contentType": u"video/mpeg"
-    }, 
-    u".doc": {
-         
-        u"isText": False, 
-        u"contentType": u"application/msword"
-    }, 
-    u".pgm": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-portable-graymap"
-    }, 
-    u".pot": {
-         
-        u"isText": False, 
-        u"contentType": u"application/vnd.ms-powerpoint"
-    }, 
-    u".mif": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-mif"
-    }, 
-    u".roff": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-troff"
     }, 
     u".htm": {
         u"isText": True, 
@@ -676,94 +241,26 @@ default_types = {
         u"isText": True, 
         u"contentType": u"text/html"
     }, 
-    u".man": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-troff-man"
-    }, 
-    u".etx": {
-         
-        u"isText": False, 
-        u"contentType": u"text/x-setext"
-    }, 
     u".zip": {
-         
         u"isText": False, 
         u"contentType": u"application/zip"
     }, 
-    u".movie": {
-         
-        u"isText": False, 
-        u"contentType": u"video/x-sgi-movie"
-    }, 
-    u".pyc": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-python-code"
-    }, 
     u".png": {
-         
         u"isText": False, 
         u"contentType": u"image/png"
     }, 
     u".svg": {
-        u"isText": False, 
+        u"isText": True, 
         u"contentType": u"image/svg+xml"
     },
     u".svge": {
-        u"isText": False, 
+        u"isText": True, 
         u"contentType": u"image/svg+xml"
     },
-    u".pfx": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-pkcs12"
-    }, 
-    u".mhtml": {
-         
-        u"isText": False, 
-        u"contentType": u"message/rfc822"
-    }, 
-    u".tar": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-tar"
-    }, 
-    u".pnm": {
-         
-        u"isText": False, 
-        u"contentType": u"image/x-portable-anymap"
-    }, 
-    u".pyo": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-python-code"
-    }, 
-    u".snd": {
-         
-        u"isText": False, 
-        u"contentType": u"audio/basic"
-    }, 
-    u".cpio": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-cpio"
-    }, 
-    u".swf": {
-         
-        u"isText": False, 
-        u"contentType": u"application/x-shockwave-flash"
-    }, 
     u".mp3": {
-         
         u"isText": False, 
         u"contentType": u"audio/mpeg"
     }, 
-    u".mp2": {
-         
-        u"isText": False, 
-        u"contentType": u"audio/mpeg"
-    },
     u".caty": {
         u"isText": True,
         u"contentType": u""
@@ -773,7 +270,6 @@ default_types = {
         u"contentType": u""
     },
     u".json": {
-        
         u"isText": True,
         u"contentType": u"application/json"
     },
@@ -785,31 +281,28 @@ default_types = {
         u"isText": True,
         u"contentType": u""
     },
-    u".pcasm": {
-        u"isText": True,
-        u"contentType": u""
-    },
-
-
     u".xjson": {
-        
         u"isText": True,
         u"contentType": u"application/json"
     },
-
     u".csv": {
-        
         u"isText": True,
         u"contentType": u"text/csv"
     },
     # 一応、念のため
     u".cgi": {
-        
+        u"isText": True,
+        u"contentType": u"text/plain"
+    },
+    u".eps": {
+        u"isText": True,
+        u"contentType": u"text/plain"
+    },
+    u".dot": {
         u"isText": True,
         u"contentType": u"text/plain"
     },
     u".beh": {
-        
         u"isText": True,
         u"contentType": u"text/plain"
     },
@@ -818,6 +311,26 @@ default_types = {
         u"contentType": u"text/plain"
     },
     u".lit": {
+        u"isText": True,
+        u"contentType": u"text/plain"
+    },
+    u".tar": {
+        u"isText": False,
+        u"contentType": u"application/x-tar"
+    },
+    u".gz": {
+        u"isText": False,
+        u"contentType": u"application/gzip"
+    },
+    u".ogg": {
+        u"isText": False,
+        u"contentType": u"application/ogg"
+    },
+    u".bmp": {
+        u"isText": False,
+        u"contentType": u"image/bmp"
+    },
+    u".tex": {
         u"isText": True,
         u"contentType": u"text/plain"
     },
