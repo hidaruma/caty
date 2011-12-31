222fb56aa1d5ebe2514572bd6562ca46d32185ed
m_hiyama <devnull@localhost>
2011-12-31 18:44:52 +0900

    gvに追加と変更、setモジュール、pred（プレディケート）モジュールを追加

---
+++ global/commands/pred.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*- 
+# 
+#
+from caty.command import Command
+import caty.jsontools as json
+
+def _result(tagged, result):
+    if tagged:
+        return json.tagged(result, None)
+    else:
+        return result
+
+class Or(Command):
+    def setup(self, opts):
+        self.tagged = opts['tagged']
+
+    def execute(self, input):
+        for item in input:
+            if item == u'True':
+                return _result(self.tagged, u'True')
+        return _result(self.tagged, u'False')
+
+class And(Command):
+    def setup(self, opts):
+        self.tagged = opts['tagged']
+
+    def execute(self, input):
+        for item in input:
+            if item == u'False':
+                return _result(self.tagged, u'False')
+        return _result(self.tagged, u'True')
+
+class TagOr(Command):
+    def setup(self, opts):
+        self.tagged = opts['tagged']
+
+    def execute(self, input):
+        for item in input:
+            if json.tag(item) == 'True':
+                return _result(self.tagged, u'True')
+        return _result(self.tagged, u'False')
+
+class TagAnd(Command):
+    def setup(self, opts):
+        self.tagged = opts['tagged']
+
+    def execute(self, input):
+        for item in input:
+            if json.tag(item) == 'False':
+                return _result(self.tagged, u'False')
+        return _result(self.tagged, u'True')
+
+class BAnd(Command):
+    def setup(self, opts):
+        self.tagged = opts['tagged']
+
+    def execute(self, input):
+        for item in input:
+            if item == False:
+                return _result(self.tagged, u'False')
+        return _result(self.tagged, u'True')
+
+class BOr(Command):
+    def setup(self, opts):
+        self.tagged = opts['tagged']
+
+    def execute(self, input):
+        for item in input:
+            if item == True:
+                return _result(self.tagged, u'True')
+        return _result(self.tagged, u'False')
