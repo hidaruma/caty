3379c27409a2f076523fd6298a780e6b6f0453d4
ckuwata <devnull@localhost>
2012-01-05 16:47:09 +0900

    Issue #291がまだ直ってなかったので修正

--- global/commands/gv.py
+++ global/commands/gv.py
@@ -79,6 +79,8 @@ class Draw(Builtin):
         G = AGraph(name=name, directed=d, strict=s)
         if self._font:
             G.graph_attr.update(fontname=self._font)
+            G.node_attr.update(fontname=self._font)
+            G.edge_attr.update(fontname=self._font)
         G.graph_attr.update(graph.get('graph', {}))
         G.node_attr.update(graph.get('node', {}))
         G.edge_attr.update(graph.get('edge', {}))
