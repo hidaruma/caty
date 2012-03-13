1cc5debff985581e7a73be43840683d9f5b37428
ckuwata <devnull@localhost>
2012-03-13 16:37:12 +0900

    inspectモジュールを追加

---
+++ python/caty/core/std/command/inspect.py
@@ -0,0 +1,45 @@
+#coding: utf-8
+from caty.core.command import Internal
+from caty.core.exception import *
+class ListCommands(Internal):
+    def setup(self, opts, module_name):
+        self._short = opts['short']
+        self._module_name = module_name
+
+    def execute(self):
+        if ':' in self._module_name:
+            app_name, mod_name = self._module_name.split(':', 1)
+            if app_name == 'this':
+                app_name = self.current_app.name
+        else:
+            app_name = self.current_app.name
+            mod_name = self._module_name
+        app = self._system.get_app(app_name)
+        mod = app._schema_module.get_module(mod_name)
+        r = []
+        for k, v in mod.proto_ns.items():
+            if v.module.name == mod.name:
+                r.extend(self._get_command_info(v))
+        return r
+
+    def _get_command_info(self, proto_type):
+        from caty.core.casm.cursor.dump import TreeDumper
+        td = TreeDumper()
+        td.started = True
+        profiles = []
+        for p in proto_type.patterns:
+            o = {
+                'name': proto_type.name,
+                'implemented': proto_type.script_proxy is not None or hasattr(proto_type.uri, 'python') and proto_type.uri.python != 'caty.core.command.Dummy',
+            }
+            o['opts'] = td.visit(p.opts) if p.opts else u'void'
+            o['args'] = td.visit(p.args) if p.args else u'void'
+            o['input'] = td.visit(p.decl.profiles[0][0])
+            o['output'] = td.visit(p.decl.profiles[0][1])
+            o['throws'] = []
+            for ls in p.decl.jump:
+                for node in ls:
+                    o['throws'].append(td.visit(node))
+            profiles.append(o)
+        return profiles
+        
---
+++ python/caty/core/std/schema/inspect.py
@@ -0,0 +1,56 @@
+#coding: utf-8
+name = u'inspect'
+schema = u"""
+type typeExprText = string(remark="型表現のテキスト");
+type exceptionName = string(remark="例外の名前");
+
+
+/** コマンドプロファイル情報の簡易版 */
+type ShortProfile = {
+ /** コマンドの名前 */
+ "name" : string,
+
+ /** 実装を持つか 
+  * 宣言だけのときはfalse
+  */
+ "implemented" : boolean?,
+
+ /** オプションの型 */
+ "opts" : typeExprText,
+
+ /** 引数の型 
+  * arg0は含まれないので、args[0]がargv[1]であることに注意
+  */
+ "args" : typeExprText,
+
+ /** 入力の型 */
+ "input" : typeExprText,
+
+ /** 出力の型 */
+ "output" : typeExprText,
+
+ /** 例外の型 */
+ "throws" : ([exceptionName*] | @only [exceptionName*])?,
+};
+
+/** モジュールに含まれるコマンドを列挙する
+ * 引数に指定されたモジュールに固有なコマンドだけを列挙する。
+ * 別名として存在するコマンドや、そのモジュールから可視な別モジュールのコマンドは列挙しない。
+ * 
+ * モジュール名はパッケージ修飾を許す。
+ * またアプリケーション名で修飾してもよい。
+ * 例：
+ *  * mymod
+ *  * pkg.mymod
+ *  * otherapp:somepkg.yourmod
+ *  * this:pkg.mymod
+ */
+command list 
+ {
+   /** 当面、常にshortオプションを要求する */
+   "short" : true,
+ }
+ [string moduleName] :: void -> [ShortProfile*]
+ throws ModuleNotFound
+ refers python:caty.core.std.command.inspect.ListCommands;
+"""
