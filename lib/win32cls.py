4299d939f9d4bf825c6ea43914fedfddbc59ec9c
m_hiyama <devnull@localhost>
2012-04-16 08:35:01 +0900

    Win32とctypesの問題をfix。

--- lib/win32cls.py
+++ lib/win32cls.py
@@ -4,7 +4,7 @@
 # see http://d.hatena.ne.jp/m-hiyama/20091222/1261444695
 #
 
-from ctypes import windll, Structure, c_short, c_ushort, c_uint, byref
+from ctypes import windll, Structure, c_short, c_ushort, c_int, c_uint, byref
 
 # Win32の型の定義: 
 # SHORT, WORD, DWORD, 
@@ -50,21 +50,31 @@ FillConsoleOutputCharacterA = windll.kernel32.FillConsoleOutputCharacterA
 FillConsoleOutputAttribute = windll.kernel32.FillConsoleOutputAttribute
 SetConsoleCursorPosition = windll.kernel32.SetConsoleCursorPosition
 
+# pyreadline-1.7.1/pyreadline/console/cosole.py
+#
+#       Copyright (C) 2003-2006 Gary Bishop.
+#       Copyright (C) 2006  Jorgen Stenarson. <jorgen.stenarson@bostream.nu>
+#
+#  Distributed under the terms of the BSD License.
+def fixcoord(x, y):
+    u'''Return a long with x and y packed inside.'''
+    
+    # this is a hack! ctypes won't pass structures but COORD is 
+    # just like a long, so this works.
+    return c_int(y << 16 | x)
+
 # 画面クリア
 
 def clear_screen ():
   csbi = CONSOLE_SCREEN_BUFFER_INFO()
-  coord = COORD() # 初期値 coord.X = 0, coord.Y = 0
+  coord = fixcoord(0, 0) # 初期値 coord.X = 0, coord.Y = 0
   dwDummy = DWORD()
 
   hConOut = stdout_handle
   if GetConsoleScreenBufferInfo (hConOut, byref(csbi)):
-    import pdb; pdb.set_trace()
     FillConsoleOutputCharacterA (hConOut, 32, 
                                  csbi.dwSize.X * csbi.dwSize.Y, 
                                  coord, byref(dwDummy))
-    import pdb; pdb.set_trace()
-    coord = COORD()
     FillConsoleOutputAttribute (hConOut, csbi.wAttributes, 
                                 csbi.dwSize.X * csbi.dwSize.Y, 
                                 coord, byref(dwDummy))
