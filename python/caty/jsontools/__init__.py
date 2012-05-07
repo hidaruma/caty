04d204cb2663efad5f6b8de2944d6f211c1f0d6e
ckuwata <devnull@localhost>
2012-05-07 21:21:37 +0900

    Python2.7でjsonのエンコーダ仕様が変わったので、対応コードを追加

--- python/caty/jsontools/__init__.py
+++ python/caty/jsontools/__init__.py
@@ -58,6 +58,8 @@ def dump(obj, fo, **kwds):
 def dumps(obj, **kwds):
     return json.dumps(encode(obj), cls=CatyEncoder, **kwds)
 
+import sys
+_is26 = sys.version.startswith('2.6')
 class CatyEncoder(json.encoder.JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
@@ -71,14 +73,252 @@ class CatyEncoder(json.encoder.JSONEncoder):
         else:
             return json.encoder.JSONEncoder.encode(self, o)
 
-    def _iterencode(self, o, markers=None):
-        if isinstance(o, str):
-            yield u'b"%s"' % repr(o)[1:-1]
-        elif o is UNDEFINED:
-            pass
-        else:
-            for v in json.encoder.JSONEncoder._iterencode(self, o, markers):
-                yield v
+    if _is26:
+        def _iterencode(self, o, markers=None):
+            if isinstance(o, str):
+                yield u'b"%s"' % repr(o)[1:-1]
+            elif o is UNDEFINED:
+                pass
+            else:
+                for v in json.encoder.JSONEncoder._iterencode(self, o, markers):
+                    yield v
+    else:
+
+        def iterencode(self, o, _one_shot=False):
+            """Encode the given object and yield each string
+            representation as available.
+
+            For example::
+
+                for chunk in JSONEncoder().iterencode(bigobject):
+                    mysocket.write(chunk)
+
+            """
+            from json.encoder import FLOAT_REPR, INFINITY, encode_basestring, encode_basestring_ascii
+            if self.check_circular:
+                markers = {}
+            else:
+                markers = None
+            if self.ensure_ascii:
+                _encoder = encode_basestring_ascii
+            else:
+                _encoder = encode_basestring
+            def floatstr(o, allow_nan=self.allow_nan,
+                    _repr=FLOAT_REPR, _inf=INFINITY, _neginf=-INFINITY):
+                # Check for specials.  Note that this type of test is processor
+                # and/or platform-specific, so do tests which don't depend on the
+                # internals.
+
+                if o != o:
+                    text = 'NaN'
+                elif o == _inf:
+                    text = 'Infinity'
+                elif o == _neginf:
+                    text = '-Infinity'
+                else:
+                    return _repr(o)
+
+                if not allow_nan:
+                    raise ValueError(
+                        "Out of range float values are not JSON compliant: " +
+                        repr(o))
+
+                return text
+
+
+            _iterencode = self._make_iterencode(
+                markers, self.default, _encoder, self.indent, floatstr,
+                self.key_separator, self.item_separator, self.sort_keys,
+                self.skipkeys, _one_shot, self._iterencode)
+            return _iterencode(o, 0)
+
+        def _iterencode(self, o, _current_indent_level, _iterencode_list, _iterencode_dict):
+            if isinstance(o, unicode):
+                yield _encoder(o)
+            elif isinstance(o, str):
+                yield 'b"%s"' % repr(o)[1:-1]
+            elif o is None:
+                yield 'null'
+            elif o is True:
+                yield 'true'
+            elif o is False:
+                yield 'false'
+            elif isinstance(o, (int, long)):
+                yield str(o)
+            elif isinstance(o, float):
+                yield _floatstr(o)
+            elif isinstance(o, (list, tuple)):
+                for chunk in _iterencode_list(o, _current_indent_level):
+                    yield chunk
+            elif isinstance(o, dict):
+                for chunk in _iterencode_dict(o, _current_indent_level):
+                    yield chunk
+            elif isinstance(o, UNDEFINED):
+                pass
+            else:
+                if markers is not None:
+                    markerid = id(o)
+                    if markerid in markers:
+                        raise ValueError("Circular reference detected")
+                    markers[markerid] = o
+                o = _default(o)
+                for chunk in _iterencode(o, _current_indent_level):
+                    yield chunk
+                if markers is not None:
+                    del markers[markerid]
+
+    def _make_iterencode(self, markers, _default, _encoder, _indent, _floatstr,
+            _key_separator, _item_separator, _sort_keys, _skipkeys, _one_shot, iterencode,
+            ## HACK: hand-optimized bytecode; turn globals into locals
+            ValueError=ValueError,
+            basestring=basestring,
+            dict=dict,
+            float=float,
+            id=id,
+            int=int,
+            isinstance=isinstance,
+            list=list,
+            long=long,
+            str=str,
+            tuple=tuple,
+        ):
+
+        def _iterencode_list(lst, _current_indent_level):
+            if not lst:
+                yield '[]'
+                return
+            if markers is not None:
+                markerid = id(lst)
+                if markerid in markers:
+                    raise ValueError("Circular reference detected")
+                markers[markerid] = lst
+            buf = '['
+            if _indent is not None:
+                _current_indent_level += 1
+                newline_indent = '\n' + (' ' * (_indent * _current_indent_level))
+                separator = _item_separator + newline_indent
+                buf += newline_indent
+            else:
+                newline_indent = None
+                separator = _item_separator
+            first = True
+            for value in lst:
+                if first:
+                    first = False
+                else:
+                    buf = separator
+                if isinstance(value, basestring):
+                    yield buf + _encoder(value)
+                elif value is None:
+                    yield buf + 'null'
+                elif value is True:
+                    yield buf + 'true'
+                elif value is False:
+                    yield buf + 'false'
+                elif isinstance(value, (int, long)):
+                    yield buf + str(value)
+                elif isinstance(value, float):
+                    yield buf + _floatstr(value)
+                else:
+                    yield buf
+                    if isinstance(value, (list, tuple)):
+                        chunks = _iterencode_list(value, _current_indent_level)
+                    elif isinstance(value, dict):
+                        chunks = _iterencode_dict(value, _current_indent_level)
+                    else:
+                        chunks = _iterencode(value, _current_indent_level)
+                    for chunk in chunks:
+                        yield chunk
+            if newline_indent is not None:
+                _current_indent_level -= 1
+                yield '\n' + (' ' * (_indent * _current_indent_level))
+            yield ']'
+            if markers is not None:
+                del markers[markerid]
+
+        def _iterencode_dict(dct, _current_indent_level):
+            if not dct:
+                yield '{}'
+                return
+            if markers is not None:
+                markerid = id(dct)
+                if markerid in markers:
+                    raise ValueError("Circular reference detected")
+                markers[markerid] = dct
+            yield '{'
+            if _indent is not None:
+                _current_indent_level += 1
+                newline_indent = '\n' + (' ' * (_indent * _current_indent_level))
+                item_separator = _item_separator + newline_indent
+                yield newline_indent
+            else:
+                newline_indent = None
+                item_separator = _item_separator
+            first = True
+            if _sort_keys:
+                items = sorted(dct.items(), key=lambda kv: kv[0])
+            else:
+                items = dct.iteritems()
+            for key, value in items:
+                if isinstance(key, basestring):
+                    pass
+                # JavaScript is weakly typed for these, so it makes sense to
+                # also allow them.  Many encoders seem to do something like this.
+                elif isinstance(key, float):
+                    key = _floatstr(key)
+                elif key is True:
+                    key = 'true'
+                elif key is False:
+                    key = 'false'
+                elif key is None:
+                    key = 'null'
+                elif isinstance(key, (int, long)):
+                    key = str(key)
+                elif _skipkeys:
+                    continue
+                else:
+                    raise TypeError("key " + repr(key) + " is not a string")
+                if first:
+                    first = False
+                else:
+                    yield item_separator
+                yield _encoder(key)
+                yield _key_separator
+                if isinstance(value, basestring):
+                    yield _encoder(value)
+                elif value is None:
+                    yield 'null'
+                elif value is True:
+                    yield 'true'
+                elif value is False:
+                    yield 'false'
+                elif isinstance(value, (int, long)):
+                    yield str(value)
+                elif isinstance(value, float):
+                    yield _floatstr(value)
+                else:
+                    if isinstance(value, (list, tuple)):
+                        chunks = _iterencode_list(value, _current_indent_level)
+                    elif isinstance(value, dict):
+                        chunks = _iterencode_dict(value, _current_indent_level)
+                    else:
+                        chunks = _iterencode(value, _current_indent_level)
+                    for chunk in chunks:
+                        yield chunk
+            if newline_indent is not None:
+                _current_indent_level -= 1
+                yield '\n' + (' ' * (_indent * _current_indent_level))
+            yield '}'
+            if markers is not None:
+                del markers[markerid]
+            
+        _iterencode = lambda o, _current_indent_level: iterencode(o, 
+                                                           _current_indent_level, 
+                                                           _iterencode_list, 
+                                                           _iterencode_dict)
+        return _iterencode
+
+
 
 class InternalDecimal(float):
     u"""simplejson の JSONEncoder は、 default をオーバーライドした結果をさらにエンコードするようになっている。
@@ -107,25 +347,46 @@ class PPEncoder(CatyEncoder):
         else:
             return CatyEncoder.default(self, o)
     
-    def _iterencode(self, o, markers=None):
-        if isinstance(o, TaggedValue):
-            t, v = split_tag(o)
-            yield u'@%s ' % t
-            for e in self._iterencode(v, markers):
-                yield e
-        elif isinstance(o, TagOnly):
-            yield u'@%s' % (o.tag)
-        elif not isinstance(o, (basestring, bool, int, decimal.Decimal, InternalDecimal, dict, list, tuple)) and o is not None:
-            from caty import UNDEFINED
-            if o is UNDEFINED:
-                yield u"#'undefined"
-            elif o is _empty:
-                yield u''
+    if _is26:
+        def _iterencode(self, o, markers=None):
+            if isinstance(o, TaggedValue):
+                t, v = split_tag(o)
+                yield u'@%s ' % t
+                for e in self._iterencode(v, markers):
+                    yield e
+            elif isinstance(o, TagOnly):
+                yield u'@%s' % (o.tag)
+            elif not isinstance(o, (basestring, bool, int, decimal.Decimal, InternalDecimal, dict, list, tuple)) and o is not None:
+                from caty import UNDEFINED
+                if o is UNDEFINED:
+                    yield u"#'undefined"
+                elif o is _empty:
+                    yield u''
+                else:
+                    yield u'#<foreign %s>' % repr(o)
             else:
-                yield u'#<foreign %s>' % repr(o)
-        else:
-            for e in CatyEncoder._iterencode(self, self.__normalize(o), markers):
-                yield e
+                for e in CatyEncoder._iterencode(self, self.__normalize(o), markers):
+                    yield e
+    else:
+        def _iterencode(self, o, _current_indent_level, _iterencode_list, _iterencode_dict):
+            if isinstance(o, TaggedValue):
+                t, v = split_tag(o)
+                yield u'@%s ' % t
+                for e in self._iterencode(v, _current_indent_level, _iterencode_list, _iterencode_dict):
+                    yield e
+            elif isinstance(o, TagOnly):
+                yield u'@%s' % (o.tag)
+            elif not isinstance(o, (basestring, bool, int, decimal.Decimal, InternalDecimal, dict, list, tuple)) and o is not None:
+                from caty import UNDEFINED
+                if o is UNDEFINED:
+                    yield u"#'undefined"
+                elif o is _empty:
+                    yield u''
+                else:
+                    yield u'#<foreign %s>' % repr(o)
+            else:
+                for e in CatyEncoder._iterencode(self, self.__normalize(o), _current_indent_level, _iterencode_list, _iterencode_dict):
+                    yield e
 
     def __normalize(self, o):
         if isinstance(o, (tuple, list)):
