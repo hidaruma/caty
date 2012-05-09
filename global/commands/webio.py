6ea43b5cb96e6401dce4e5ed15d67a054fad7400
ckuwata <devnull@localhost>
2012-05-09 18:22:22 +0900

    webio:unparseを実装

--- global/commands/webio.py
+++ global/commands/webio.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
+
 from caty.command import Command
 from caty.core.exception import throw_caty_exception
-from caty.jsontools import tagged, obj2path, untagged, split_tag, path2obj
+from caty.jsontools import tagged, obj2path, untagged, split_tag, path2obj, prettyprint
 from caty.core.std.command.builtin import TypeCalculator
 from caty.core.schema import JsonSchemaError
 
@@ -58,7 +60,7 @@ class Translate(Command, TypeCalculator):
                 t, v = split_tag(generic_data)
                 scm = self.converter
                 if t == 'form':
-                    return scm.convert(self._to_nested(v))
+                    return scm.convert(_to_nested(v))
                 elif t == 'json':
                     n = scm.fill_default(v)
                     scm.validate(n)
@@ -76,8 +78,64 @@ class Translate(Command, TypeCalculator):
                 else:
                     throw_caty_exception('ConversionError', u'failed to convert data:\n$errorObj\nsource:$source', errorObj=er, source=generic_data)
 
-    def _to_nested(self, input):
-        d = {}
-        for k, v in input.items():
-            d['$.'+k] = v
-        return path2obj(d) if d else d
+def _to_nested(input):
+    d = {}
+    for k, v in input.items():
+        d['$.'+k] = v
+    return path2obj(d) if d else d
+
+class Unparse(Command):
+    def setup(self, opts):
+        self.__content_type = opts.get('content-type')
+
+    def execute(self, generic_data):
+        tag, data = split_tag(generic_data)
+        if self.__content_type is None:
+            if tag == 'form':
+                self.__content_type = 'application/x-www-form-urlencoded'
+            elif tag == 'json':
+                self.__content_type = 'application/json'
+            elif tag == 'text':
+                self.__content_type = 'text/plain'
+            else:
+                self.__content_type = 'application/octet-stream'
+        if self.__content_type == 'application/x-www-form-urlencoded':
+            if isinstance(data, object):
+                return self._encode_to_form(data)
+            else:
+                raise
+        elif self.__content_type == 'multipart/form-data':
+            raise NotImplementedError()
+        elif self.__content_type == 'application/json':
+            return prettyprint(data)
+        elif self.__content_type.startswith('text/'):
+            if isinstance(data, basestring):
+                return data
+            elif isinstance(data, bool):
+                return str(bool).lower()
+            elif isinstance(data, (int, Decimal)):
+                return str(data)
+            else:
+                return prettyprint(data)
+        else:
+            if isinstance(data, str):
+                return data
+            elif isinstance(data, unicode):
+                return data.encode(self.env.get('APP_ENCODING', 'utf-8'))
+            elif isinstance(data, bool):
+                return str(bool).lower()
+            elif isinstance(data, (int, Decimal)):
+                return str(data)
+            else:
+                return prettyprint(data).encode(self.env.get('APP_ENCODING', 'utf-8'))
+
+    def _encode_to_form(self, data):
+        import urllib
+        r = []
+        for k, v in data.items():
+            for i in v:
+                if isinstance(i, unicode):
+                    i = i.encode(self.env.get('APP_ENCODING', 'utf-8'))
+                r.append(urllib.urlencode({k: i}))
+        return ''.join(r)
+
