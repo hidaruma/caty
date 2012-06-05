f3e23a886516fc243ac6ba06028ac70d19925f2e
ckuwata <devnull@localhost>
2012-06-05 17:44:44 +0900

    DEBUG環境変数のデフォルト値がnullになっていたのを修正

--- python/caty/front/util.py
+++ python/caty/front/util.py
@@ -24,7 +24,7 @@ def make_base_opt_parser(mode):
     parser.add_option('-s', '--system-encoding', dest='system_encoding', default=_get_encoding(), help=u'システムのエンコーディングを指定する')
     parser.add_option('-a', '--app', dest='apps', metavar='APP', action='append', help=u'APPを起動後のデフォルトアプリケーションにする\n複数指定された場合、最初のものがデフォルトに使われる')
     parser.add_option('--force-app', dest='force_app', metavar='APP', help=u'APPを_manifest.xjsonの記述に関わらず強制的に起動する')
-    parser.add_option('-d', '--debug',action='store_true', help=u'デバッグモードで起動する')
+    parser.add_option('-d', '--debug',action='store_true', default=False, help=u'デバッグモードで起動する')
     parser.add_option('--goodbye', action='store_true', help=u'起動直後に終了する')
     parser.add_option('--no-ambient', dest='no_ambient', action='store_true', help=u'スキーマやアクションを読み込まずに起動する')
     parser.add_option('--no-app', dest='no_app', action='store_true', help=u'起動時に--appで指定されたなかったアプリケーションを読み込まない\n--appが未指定の場合、rootだけが読み込まれる')
