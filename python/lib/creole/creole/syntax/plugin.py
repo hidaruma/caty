2fb6bb3640ff70697312edd45e02a77cc4a7872f
ckuwata <devnull@localhost>
2012-08-13 17:34:41 +0900

    Issue #869を実装

--- python/lib/creole/creole/syntax/plugin.py
+++ python/lib/creole/creole/syntax/plugin.py
@@ -8,27 +8,8 @@ class BlockPlugin(BlockParser):
         skip_ws(seq)
         name = seq.parse(Regex('[a-zA-Z][a-zA-Z0-9_-]*'))
         skip_ws(seq)
-        raw_arg = until(u'>>')(seq)
-        S('>>')(seq)
+        raw_arg = until(u'>>')(seq).strip()
+        line_by_itself('>>')(seq)
         skip_ws(seq)
         return self.syntax.call_plugin(name, raw_arg)
-
-class InlinePlugin(WikiParser):
-    
-    def __init__(self, syntax, element_factory=None):
-        WikiParser.__init__(self, element_factory)
-        self.__syntax = syntax
-        self.start = u'<<'
-        self.end = u'>>'
-
-    def __call__(self, seq, s):
-        content = until([self.end, '\n'])(seq)
-        choice(self.end, '\n')(seq)
-        if not content:
-            return u'<<>>'
-        if ' ' not in content:
-            return self.__syntax.call_plugin(content)
-        name, raw_args = content.split(' ', 1)
-        return self.__syntax.call_plugin(name.strip(), raw_args)
-
 
