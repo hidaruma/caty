3dfe55b5e05fbad1cb8a59f8018f6a21328eba89
ckuwata <devnull@localhost>
2012-08-24 10:27:09 +0900

    sreif:show-commandを実装

--- global/commands/sreif.py
+++ global/commands/sreif.py
@@ -392,6 +392,23 @@ class ShowType(SafeReifier):
         module = app._schema_module.get_module(module_name)
         return reifier.reify_type(module.get_type(name))
 
+class ShowCommand(SafeReifier):
+
+    def _execute(self):
+        reifier = ShallowReifier()
+        system = self.current_app._system
+        app_name, module_name, name = split_colon_dot_path(self._cdpath)
+        if not app_name:
+            app = self.current_app
+        else:
+            app = system.get_app(app_name)
+        if not module_name:
+            throw_caty_exception('BadArg', u'$arg', arg=self._cdpath)
+        if not name:
+            throw_caty_exception('BadArg', u'$arg', arg=self._cdpath)
+        module = app._schema_module.get_module(module_name)
+        return reifier.reify_command(module.get_command(name))
+
 class ShowState(SafeReifier):
 
     def _execute(self):
