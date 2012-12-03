a07c34d45dd60498d9b8435a542cb64d18fdd30b
m_hiyama <devnull@localhost>
2012-12-03 09:45:18 +0900

    まだ不完全だが、car.sh, cdm.sh を追加。

--- tools/execme.py
+++ tools/execme.py
@@ -1,4 +1,13 @@
 import sys
+import subprocess
+
+def check_pip():
+    try:
+        p = subprocess.Popen(['pip'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        return True
+    except:
+        return False
+
 def main() :
     major = sys.version_info[0]
     minor = sys.version_info[1]
@@ -7,7 +16,13 @@ def main() :
         sys.exit(1)
     else:
         print "OK, right Python version for Caty, \n%s." % sys.version
-        sys.exit(0)
+        print ""
+        if check_pip():
+            print "OK, pip is installed."
+            sys.exit(0)            
+        else:
+            print "Sorry, pip is NOT installed."
+            sys.exit(1)
 
 if __name__ == "__main__":
     main()
