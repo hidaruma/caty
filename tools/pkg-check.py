49346334d3c5b392d357c8a27fa7d5c33e435e09
ckuwata <devnull@localhost>
2012-12-06 10:31:59 +0900

    Issue #1352に対応

--- tools/pkg-check.py
+++ tools/pkg-check.py
@@ -1,20 +1,46 @@
 import subprocess
 import sys
+import json
 from optparse import OptionParser
 
-example = '''pkg-list-file example:
+example = """pkg-list-file example(plain text):
 
 requests==0.12.0
 pygraphviz==1.1
 PIL==1.1.7
-'''
+
+or (json)
+
+{
+  "dependencies": {
+    "python2": {
+       "requests": "0.12.0",
+       "pygraphviz": "1.1",
+       "PIL": "1.1.7"
+    }
+  },
+  :
+  :
+}
+"""
 
 def main():
     argv = sys.argv
     o = OptionParser(usage='usage: python %s [OPTIONS] pkg-list-file\n%s' % (argv[0], example))
     o.add_option('--verbose', action='store_true', default=None)
     options, args = o.parse_args(argv[1:])
-    p = subprocess.Popen(['pip', 'freeze'], stdout=subprocess.PIPE).communicate()
+    try:
+        subp = subprocess.Popen(['pip', 'freeze'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except OSError:
+        print '[Error] pip is not installed'
+        sys.exit(1)
+    r = subp.wait()
+    if r != 0:
+        p = subp.communicate()
+        print '[Error]'
+        print p
+        sys.exit(r)
+    p = subp.communicate()
     pkgmap = {}
     for l in p[0].split('\n'):
         if l:
@@ -25,18 +51,19 @@ def main():
         o.print_help()
         sys.exit(1)
     f = args[0]
+    if f.endswith('.json'):
+        extractor = extract_from_json
+    else:
+        extractor = extract_from_text
     ok = True
-    for l in open(f):
-        l = l.strip()
-        if l:
-            pkg, version = map(lambda s: s.strip(), l.split('=='))
-            if pkg in pkgmap:
-                if newer(version, pkgmap[pkg]):
-                    if options.verbose:
-                        print '[OK]', pkg, pkgmap[pkg]
-                    continue
-            ok = False
-            print '[NG]', pkg, version, 'is not installed.'
+    for pkg, version in extractor(f):
+        if pkg in pkgmap:
+            if newer(version, pkgmap[pkg]):
+                if options.verbose:
+                    print '[OK]', pkg, pkgmap[pkg]
+                continue
+        ok = False
+        print '[NG]', pkg, version, 'is not installed.'
     return ok
 
 def newer(v1, v2):
@@ -47,6 +74,17 @@ def newer(v1, v2):
             return cmp(len(a), len(b))
         return cmp(a, b)
     return 1
+
+def extract_from_text(f):
+    for l in open(f):
+        l = l.strip()
+        if l:
+            pkg, version = map(lambda s: s.strip(), l.split('=='))
+            yield pkg, version
+
+def extract_from_json(f):
+    j = json.loads(open(f).read())
+    return j.get('dependencies', {}).get('python2', {}).items()
 
 if __name__ == '__main__':
     if main():
