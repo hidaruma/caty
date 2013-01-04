4b182444ea4d6f77586e3a5840b4f93a8f3d48ef
ckuwata <devnull@localhost>
2013-01-04 16:59:41 +0900

    prj-manifestにsessionの項目がなかった場合の挙動を修正

--- python/caty/core/globalconf.py
+++ python/caty/core/globalconf.py
@@ -19,7 +19,7 @@ class GlobalConfig(object):
         self._raw_data = obj
         fs = obj['mafsModule']
         self._mafs_module = fs
-        session_conf = obj.get('session', {'module': 'caty.session', 'conf': {'type':'memory', 'expire': 3600}})
+        session_conf = obj.get('session', {'module': 'caty.session.memory', 'conf': {'expire': 3600}})
         secret_key = obj['secretKey']
         self.session = session.initialize(session_conf)
         self._filetypes = mafs.metadata.default_types
