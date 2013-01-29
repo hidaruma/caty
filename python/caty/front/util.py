9dac3ac483c164170414dc80897e7751f9e0a36f
ckuwata <devnull@localhost>
2013-01-29 12:27:18 +0900

    --goodbye --no-appなどとすると期待した動作にならないので、その場合はエラーにすることにした。

--- python/caty/front/util.py
+++ python/caty/front/util.py
@@ -25,7 +25,7 @@ def make_base_opt_parser(mode):
     parser.add_option('-a', '--app', dest='apps', metavar='APP', action='append', help=u'APPを起動後のデフォルトアプリケーションにする\n複数指定された場合、最初のものがデフォルトに使われる')
     parser.add_option('--force-app', dest='force_app', metavar='APP', help=u'APPを_manifest.xjsonの記述に関わらず強制的に起動する')
     parser.add_option('-d', '--debug',action='store_true', default=False, help=u'デバッグモードで起動する')
-    parser.add_option('--goodbye', action='callback', callback=gb, help=u'起動直後に終了する', default=None, type='string', nargs=0)
+    parser.add_option('--goodbye', action='callback', callback=gb, help=u'起動直後に終了する', default=None, nargs=0, type='string')
     parser.add_option('--no-ambient', dest='no_ambient', action='store_true', help=u'スキーマやアクションを読み込まずに起動する')
     parser.add_option('--no-app', dest='no_app', action='store_true', help=u'起動時に--appで指定されたなかったアプリケーションを読み込まない\n--appが未指定の場合、rootだけが読み込まれる')
     parser.add_option('-u', '--unleash-wildcats', action='store_true', help=u'')
@@ -45,4 +45,5 @@ def gb(option, opt, value, parser):
             break
     if found:
         parser.rargs.pop(0)
-
+    else:
+        parser.error(("%s option requires an argument") % opt)
