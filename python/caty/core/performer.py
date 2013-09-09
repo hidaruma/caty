8d69c090f9a1de7516446ee5ec0484c206b9c566
ckuwata <devnull@localhost>
2013-09-09 15:02:30 +0900

    uuserver方式でパスからコマンドへのマッピングが可能になった

--- python/caty/core/performer.py
+++ python/caty/core/performer.py
@@ -31,9 +31,11 @@ class PerformerRequestHandler(RequestHandler):
         else:
             cmdnames = [method.upper(), method.lower()]
         try:
-            containerobj = self._path_to_container(path)
+            containerobj, cmd_name = self._path_to_container(path)
             if containerobj is None:
                 raise IOError(path)
+            elif cmd_name:
+                cmdname = cmd_name
             else:
                 cmdname = None
                 for c in cmdnames:
@@ -42,38 +44,37 @@ class PerformerRequestHandler(RequestHandler):
                         break
                 if not c:
                     raise IOError(path)
-                cmd = containerobj.get_command(cmdname)
-                emitter = containerobj.get_command(u'emit-normal')
-                exhandler = containerobj.get_command(u'map-exception')
-                sighandler = containerobj.get_command(u'map-signal')
-                self._env.put(u'ACTION', cmd.canonical_name)
-                if 'deprecated' in cmd.annotations:
-                    self._app._system.deprecate_logger.debug(u'path: %s verb: %s' % (path, verb))
-                executable = self._make_executable(cmd, emitter, exhandler, sighandler, containerobj, opts, args, transaction)
+
+            cmd = containerobj.get_command(cmdname)
+            emitter = containerobj.get_command(u'emit-normal')
+            exhandler = containerobj.get_command(u'map-exception')
+            sighandler = containerobj.get_command(u'map-signal')
+            self._env.put(u'ACTION', cmd.canonical_name)
+            if 'deprecated' in cmd.annotations:
+                self._app._system.deprecate_logger.debug(u'path: %s verb: %s' % (path, verb))
+            executable = self._make_executable(cmd, emitter, exhandler, sighandler, containerobj, opts, args, transaction)
         except Exception, e:
             return ExceptionAdaptor(e, self._interpreter, traceback.format_exc(), self, error_logger)
         return PipelineAdaptor(executable, self._interpreter, self, error_logger, None, transaction)
 
     def _path_to_container(self, path):
         pkg_mod_name = path.lstrip('/').replace('+/', '.')
-        cls = u''
         if '/' in pkg_mod_name:
-            pkg_mod_name, cls = pkg_mod_name.split('/', 1)
-        if cls and not cls.endswith('/'):
-            throw_caty_exception(u'HTTP_400', u'Not implemented: $path', path=path)
-        cls = cls.strip('/')
+            pkg_mod_name, cls_or_cmd = pkg_mod_name.split('/', 1)
+        cls_or_cmd = cls_or_cmd.strip('/')
         mod = self._app._schema_module.get_module(pkg_mod_name)
-        #if mod.type != 'cara' and self._app._system.public_commands != u'all':
-        #    throw_caty_exception(u'HTTP_403', u'Forbidden')
-        if cls:
-            return mod.get_class(cls)
-        else:
-            return mod
+        if cls_or_cmd:
+            if mod.has_class(cls_or_cmd):
+                throw_caty_exception(u'HTTP_400', u'Not implemented: $path', path=path)
+                return mod.get_class(cls_or_cmd), None
+        if not cls_or_cmd:
+            throw_caty_exception(u'HTTP_400', u'Not implemented: $path', path=path)
+        return mod, cls_or_cmd
 
     def _make_executable(self, profile, emitter, exhandler, sighandler, module, opts, args, transaction):
         builder = CommandBuilder(self._interpreter._facilities, module)
-        opts_ref, args_ref = self._sift_opts_and_args(profile, opts, args)
-        em_opts_ref, em_args_ref = self._sift_opts_and_args(emitter, opts, args)
+        opts_ref, args_ref = self._shift_opts_and_args(profile, opts, args)
+        em_opts_ref, em_args_ref = self._shift_opts_and_args(emitter, opts, args)
         cls = profile.get_command_class()
         if isinstance(cls, Proxy):
             obj = scriptwrapper(profile, lambda :cls.instantiate(builder))
@@ -101,7 +102,7 @@ class PerformerRequestHandler(RequestHandler):
         else:
             raise Exception(self.module._app.i18n.get(u'Invalid transaction mode: $mode', mode=str(transaction)))
 
-    def _sift_opts_and_args(self, pc, opts, args):
+    def _shift_opts_and_args(self, pc, opts, args):
         from caty.util import try_parse_to_num
         o_schm = pc.profiles[0].opts_schema
         a_schm = pc.profiles[0].args_schema
