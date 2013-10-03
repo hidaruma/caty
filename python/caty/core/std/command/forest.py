de448785cb9f5644f443f2d737c3aa9219368782
ckuwata <devnull@localhost>
2013-10-03 18:36:53 +0900

    Issue #2146のrangeとソートを実装

--- python/caty/core/std/command/forest.py
+++ python/caty/core/std/command/forest.py
@@ -26,8 +26,8 @@ class Grouping(Builtin):
 
     def _make_groups(self, cond, records):
         groups = {}
+        q = json.untagged(cond)
         if self._is_value_cond(cond):
-            q = json.untagged(cond)
             if isinstance(q, dict):
                 path = q['field']
             else:
@@ -43,12 +43,69 @@ class Grouping(Builtin):
                     groups[key] = {
                         u'id': key,
                         u'parent': None,
-                        u'childNodes': [i]
+                        u'childNodes': [[i, r]]
                     }
                 else:
-                    groups[key][u'childNodes'].append(i)
+                    groups[key][u'childNodes'].append([i, r])
+        elif self._is_range_cond(cond):
+            path = q['field']
+            s = selector.compile(path)
+            orig = q.get(u'origin', 0)
+            step = q['step']
+            for i, r in records:
+                try:
+                    v = s.select(r).next()
+                except:
+                    v = 0
+                if v < orig:
+                    continue
+                key = u'__id_%s' % (json.pp(v/step).strip(u'"'))
+                if key not in groups:
+                    groups[key] = {
+                        u'id': key,
+                        u'parent': None,
+                        u'childNodes': [[i, r]]
+                    }
+                else:
+                    groups[key][u'childNodes'].append([i, r])
+        else:
+            raise NotImplementedError()
+        if isinstance(q, dict):
+            mob = q.get(u'memberOrderBy')
+            if mob:
+                self._sort_groups(groups, mob)
+            if q.get(u'groupOrderBy', u'+') == u'-':
+                for v in groups.values():
+                    v['childNodes'].reverse()
+
+        for v in groups.values():
+            v['childNodes'] = [c[0] for c in v['childNodes']]
         return groups
 
     def _is_value_cond(self, cond):
-        return True
+        return json.tag(cond) in (u'val', u'string')
+
+    def _is_range_cond(self, cond):
+        return json.tag(cond) == u'range'
+
+
+    def _sort_groups(self, groups, mob):
+        if not isinstance(mob, list):
+            mob = [mob]
+        func = _make_comparator(mob)
+        for g in groups.values():
+            g['childNodes'].sort(func)
+        return groups
+
+def _make_comparator(key):
+    def cmp_obj(a, b):
+        r = 0
+        for k in key:
+            asc = True
+            path = selector.compile(k)
+            r = cmp(path.select(a[1]).next(), path.select(b[1]).next())
+            if r != 0:
+                return r
+        return r
+    return cmp_obj
 
