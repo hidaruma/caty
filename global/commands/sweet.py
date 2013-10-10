5dbd6a882431edffc3e27340f814d6e897e5d362
ckuwata <devnull@localhost>
2013-10-10 16:01:22 +0900

    sweet:reify-typeを実装 オプショナルかどうかの情報は、ひとまずoptionalプロパティに入れることにした。

--- global/commands/sweet.py
+++ global/commands/sweet.py
@@ -21,7 +21,7 @@ class ReifyType(SafeReifier):
         # 型の展開を行った後の物に限る。
         try:
             r = reifier.reify_type(module.get_type(name))
-        except CatyException:
+        except CatyException as e:
             throw_caty_exception(u'BadArg', u'not a sweet type: $type', type=self._cdpath)
         return r
 
@@ -54,14 +54,49 @@ class ObjectDumper(TypeBodyReifier):
                 throw_caty_exception(u'BadArg', u'not a sweet type')
             else:
                 i = untagged(t)
-                items.append(tagged(u'item', 
-                                   {
-                                    u'value': i[u'value'],
-                                    u'label': i[u'anno'].get(u'label', UNDEFINED),
-                                   }))
+                v = {u'value': i[u'value']}
+                if u'label' in i[u'anno']:
+                    v[u'label'] = i[u'anno'][u'label']
+                items.append(tagged(u'item', v))
         return tagged(u'enum', items)
+
+    def _visit_bag(self, node):
+        r = untagged(TypeBodyReifier._visit_bag(self, node))
+        types = r['items']
+        items = []
+        for bagitem in types:
+            i = untagged(bagitem)
+            t = i[u'type']
+            if tag(t) not in SINGLETON_TYPES:
+                throw_caty_exception(u'BadArg', u'not a sweet type')
+            else:
+                o = untagged(t)
+                v = {u'value': o[u'value']}
+                if u'label' in o[u'anno']:
+                    v[u'label'] = o[u'anno'][u'label']
+                v[u'minOccurs'] = i[u'minOccurs']
+                v[u'maxOccurs'] = i[u'maxOccurs']
+                items.append(tagged(u'multi-item', v))
+        return tagged(u'multi-enum', items)
 
     def _visit_option(self, node):
         r = node.body.accept(self)
         untagged(r)['optional'] = True
         return r
+
+    def _visit_symbol(self, node):
+        from caty.core.schema import TypeReference, TypeVariable
+        if isinstance(node, (TypeReference)):
+            if node.canonical_name in self._history:
+                throw_caty_exception(u'BadArg', u'not a sweet type')
+            self._history[node.canonical_name] = True
+            return node.body.accept(self)
+        elif isinstance(node, TypeVariable):
+            throw_caty_exception(u'BadArg', u'not a sweet type')
+        else:
+            return TypeBodyReifier._visit_symbol(self, node)
+
+
+
+
+
