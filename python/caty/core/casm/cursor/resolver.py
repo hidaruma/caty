9bffe02c427663cf1d076dcda8aa6e010defd3c1
ckuwata <devnull@localhost>
2013-10-11 17:34:12 +0900

    Issue #2190を修正

--- python/caty/core/casm/cursor/resolver.py
+++ python/caty/core/casm/cursor/resolver.py
@@ -2,6 +2,7 @@
 from caty.core.casm.cursor.base import *
 from caty.core.exception import SystemResourceNotFound
 from caty.core.casm.language.ast import KindReference, TypeFunctionNode
+from caty.core.casm.cursor.dump import TreeDumper
 
 class ReferenceResolver(SchemaBuilder):
     def _visit_root(self, node):
@@ -27,6 +28,14 @@ class ReferenceResolver(SchemaBuilder):
             type_args = []
             for arg in node.type_args:
                 type_args.append(arg.accept(self))
+            a1 = len([a for a in node.type_params if not isinstance(a, NamedTypeParam)])
+            a2 = len([b for b in node.type_args if not isinstance(b, NamedParameterNode)])
+            if a1 < a2:
+                if self._root_name:
+                    t = self._root_name + ' = ' + TreeDumper().visit(node)
+                else:
+                    t = TreeDumper().visit(node)
+                throw_caty_exception(u'SCHEMA_COMPILE_ERROR', u'Number of type argument does not match: $type', type=t)
             node.type_args = type_args
         elif isinstance(node, TypeVariable):
             if self.module.has_schema(node.name):
