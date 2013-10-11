63defd2a80dc6daa26100596c5b4b79157e24402
ckuwata <devnull@localhost>
2013-10-11 16:18:30 +0900

    もう少し簡潔な方法があった

--- python/caty/core/casm/cursor/normalizer.py
+++ python/caty/core/casm/cursor/normalizer.py
@@ -453,11 +453,7 @@ class TypeCalcurator(_SubNormalizer):
         res = node.body.accept(self)
         body = dereference(res)
         if body.type == u'__variable__':
-            if body.default:
-                b = node.new_node(body._default_schema).accept(self)
-                return b
-            else:
-                return node.new_node(res)
+            return node.new_node(res)
         if node.operator != u'extract':
             if body.type != 'object':
                raise CatyException(u'SchemaCompileError', 
